b4571ffaef4a1238f50a26344f1b4e4cc8a32fd0
Christian Hein <31273371+chrhein@users.noreply.github.com>
2022-10-13 14:55:30 +0200

    Add parsing library for command line arguments (#1)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,24 @@
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.required
+
+data class CLIOptions(
+    val input: String,
+    val output: String?,
+    val threads: Int = 1,
+    val debug: Boolean = false,
+    val follow: Boolean = false
+)
+
 fun main(args: Array<String>) {
-    println("Hello, World!")
-    println("Program arguments: ${args.joinToString()}")
+    val parser = ArgParser("komreg")
+    val input by parser.option(ArgType.String, shortName = "i", description = "Input file").required()
+    val output by parser.option(ArgType.String, shortName = "o", description = "Output file")
+    val threads by parser.option(ArgType.Int, shortName = "t", description = "Number of concurrent threads").default(1)
+    val debug by parser.option(ArgType.Boolean, shortName = "d", description = "Debug logging").default(false)
+    val follow by parser.option(ArgType.Boolean, shortName = "f", description = "Trail log").default(false)
+
+    parser.parse(args)
+    val cli = CLIOptions(input, output, threads, debug, follow)
 }
