c0f3bedaf905b232a9f55dd864834db99896402b
Christian Hein <31273371+chrhein@users.noreply.github.com>
2022-10-20 13:20:17 +0200

    Add mockk and spek for testing (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,13 @@ plugins {
     application
 }
 
-group = "org.example"
+val kotlinxCliVersion = "0.3.5"
+val mockkVersion = "1.13.2"
+val junitVersion = "5.9.0"
+val spekVersion = "2.0.19"
+val hamcrestVersion = "2.2"
+
+group = "no.kartverket.komreg"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -13,8 +19,14 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:$kotlinxCliVersion")
     testImplementation(kotlin("test"))
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
+    testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.hamcrest:hamcrest:$hamcrestVersion")
+    testImplementation("org.hamcrest:hamcrest-library:$hamcrestVersion")
 }
 
 tasks.test {
---
+++ src/test/kotlin/no/kartverket/komreg/UserServiceTest.kt
@@ -0,0 +1,23 @@
+package no.kartverket.komreg
+
+import io.mockk.every
+import io.mockk.mockk
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object UserServiceTest: Spek({
+
+    describe("User ID") {
+        val idGeneratorMock = mockk<IdGeneratorI>() {
+            every { getId() } returns "1"
+        }
+        val userservice = UserService(idGeneratorMock)
+        it("should be random") {
+            val newUser = userservice.createNewUser("Vegard", 32)
+            assertThat(newUser.id, equalTo("1"))
+            assertThat(userservice.getUserIdPlus1(newUser), equalTo("1_1"))
+        }
+    }
+})
