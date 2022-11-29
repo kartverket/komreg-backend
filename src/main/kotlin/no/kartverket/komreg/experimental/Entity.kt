f2852752f9bb443d6ce0b26ab2702abb984e7ae7
Alexander Solstad Larsen <489056+alexasl@users.noreply.github.com>
2022-11-29 15:32:22 +0100

    TH-153 Lagt til transformasjonssteg

--- src/main/kotlin/no/kartverket/komreg/experimental/Entity.kt
+++ src/main/kotlin/no/kartverket/komreg/experimental/Entity.kt
@@ -1,6 +1,8 @@
 package no.kartverket.komreg.experimental
 
 import no.kartverket.komreg.domain.EntityData
+import java.lang.System.Logger.Level
+import java.lang.UnsupportedOperationException
 import java.util.UUID
 import kotlin.reflect.jvm.javaType
 import kotlin.reflect.typeOf
@@ -8,10 +10,25 @@ import kotlin.reflect.typeOf
 
 interface Embeddable<out A : EntityData> {
     val id: Id<@UnsafeVariance A>
+
 }
 
 sealed interface Entity<out A : EntityData> : Embeddable<A> {
     val data: Validation<A>
+
+    fun <B> fold(ifInvalid: () -> B, ifValid: (A) -> B): B {
+        return data.fold({_, _ -> ifInvalid() }){ _, a ->
+            ifValid(a)
+        }
+    }
+}
+
+fun <FA : Entity<A>, A : EntityData> FA.transform(f: (A) -> A): FA {
+    return when (this) {
+        is DatabaseEntity<*> -> (this as DatabaseEntity<A>).copy(data = data.map(f))
+        is VirtualEntity<*> -> (this as VirtualEntity<A>).copy(data = data.map(f))
+        else -> throw UnsupportedOperationException()
+    } as FA
 }
 
 sealed interface SourceEntity<out A : EntityData> : Entity<A> {
@@ -60,4 +77,14 @@ data class GeneratedId<A : EntityData>(override val dataClass: Class<A>, val uui
             return GeneratedId(javaClass, uuid)
         }
     }
+}
+
+inline fun <reified FA : Entity<A>, reified A : EntityData> FA.log(level: Level, msg: () -> String): FA {
+    val data1 = data.log(level, null, msg())
+    val id1 = this.id
+    return when(this) {
+        is DatabaseEntity<*> -> DatabaseEntity(id1 as SourceId<A>, data1)
+        is VirtualEntity<*> -> VirtualEntity(id1 as GeneratedId<A>, data1)
+        else -> throw UnsupportedOperationException()
+    } as FA
 }
---
+++ src/main/kotlin/no/kartverket/komreg/transformation/Transform.kt
@@ -0,0 +1,88 @@
+package no.kartverket.komreg.transformation
+
+import no.kartverket.komreg.domain.EntityData
+import no.kartverket.komreg.domain.Grunneiendom
+import no.kartverket.komreg.domain.Matrikkelenhet
+import no.kartverket.komreg.experimental.*
+import java.lang.System.Logger.Level.ERROR
+
+sealed class Transform<T : EntityData>(val entity: Entity<T>) {
+    class Transformed<T : EntityData>(data: Entity<T>, val transformation: TransformFunc<T>) : Transform<T>(data)
+    class NoOp<T : EntityData>(data: Entity<T>) : Transform<T>(data)
+    //class Error<out T : EntityData>(data: T, val errors: List<String>) : Transform<T>(data)
+
+    fun transform(trans: List<TransformFunc<T>>): Transform<T> = trans.fold(this) { acc, transformFunc ->
+        acc.transform(transformFunc)
+    }
+
+    private fun transform(tf: TransformFunc<T>): Transform<T> = when (this) {
+        is NoOp -> tf.map(this.entity)
+        is Transformed -> {
+            // TODO: Legg til higher kinded types i Kotlin ;-)
+            val msgSupplier = { "Double t: ['${tf.description}', '${transformation.description}']" }
+            val entity = when(entity) {
+                is DatabaseEntity -> entity.copy(data = entity.data.log(ERROR, null,  msgSupplier))
+                is VirtualEntity -> entity.copy(data = entity.data.log(ERROR, null,  msgSupplier))
+            }
+            Transformed(
+                data = entity,
+                transformation = this.transformation
+            )
+}
+    }
+
+    override fun toString(): String = "Transform(${this.entity}, " + when (this) {
+        is NoOp -> "NoOp"
+        is Transformed -> "Transformed: '${this.transformation.description}'@${Integer.toHexString(this.hashCode())}"
+    } + ")"
+
+    companion object {
+        fun <T : EntityData> noOp(t: Entity<T>): Transform<T> = NoOp(t)
+    }
+}
+
+interface TransformFunc<T : EntityData> {
+    val description: String
+    fun map(input: Entity<T>): Transform<T>
+}
+
+class AddGardsnummerRule<T : Matrikkelenhet>(
+    private val range: IntRange,
+    private val increase: Int,
+) : TransformFunc<T> {
+    override val description: String = "Add grunn $range +$increase"
+
+    @Suppress("UNCHECKED_CAST")
+    override fun map(input: Entity<T>): Transform<T> = input.fold({ Transform.NoOp(input) }) { data ->
+        if (data is Grunneiendom && data.gardsnummer in range) {
+            Transform.Transformed(input.transform { x ->
+                (x as Grunneiendom).copy(gardsnummer = x.gardsnummer + increase) as T
+            }, this)
+        } else {
+            Transform.NoOp(input)
+        }
+    }
+
+}
+
+fun main() {
+    val rules = listOf(
+        AddGardsnummerRule<Matrikkelenhet>(2..10, 50),
+        AddGardsnummerRule(40..80, 5)
+    )
+
+    val grunneiendommer = listOf(
+        Grunneiendom(1, 10, 10, emptySet(), emptySet()),
+        Grunneiendom(1, 50, 10, emptySet(), emptySet()),
+        Grunneiendom(1, 120, 10, emptySet(), emptySet()),
+        Grunneiendom(1, 1020, 10, emptySet(), emptySet())
+    ).map { SourceEntity<Matrikkelenhet>(GeneratedId(), Valid(it)) }
+    println(grunneiendommer)
+
+    grunneiendommer
+        .map { Transform.noOp(it) }
+        .map { it.transform(rules) }
+        .onEach { println(it) }
+        .filter { it !is Transform.NoOp }
+        .map { it.entity }
+}
