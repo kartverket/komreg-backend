c8e2b632c5fc71e287c9b1224413fcea7d7ae578
Vegard Veiset <veiset@gmail.com>
2023-02-13 15:10:56 +0100

    Dummy endpoint for health

--- server/src/main/kotlin/no/kartverket/komreg/AppRoutes.kt
+++ server/src/main/kotlin/no/kartverket/komreg/AppRoutes.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 import io.ktor.server.routing.routing
@@ -54,6 +55,11 @@ fun Application.routes() {
                 }
 
                 call.respond(responseData)
+            }
+        }
+        route("/actuator/health") {
+            get {
+                call.respond("OK")
             }
         }
     }
--- server/src/main/kotlin/no/kartverket/komreg/Application.kt
+++ server/src/main/kotlin/no/kartverket/komreg/Application.kt
@@ -3,6 +3,7 @@ package no.kartverket.komreg
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.metrics.micrometer.MicrometerMetrics
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import org.rocksdb.RocksDB
 import java.util.concurrent.ForkJoinPool
@@ -18,5 +19,6 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    install(MicrometerMetrics)
     routes()
 }
