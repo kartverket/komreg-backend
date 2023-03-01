bac12e02e6fd0ca9a674f443c84b0fb752f38c7a
Christian <scheiehein@gmail.com>
2023-03-01 09:09:06 +0100

    Add method for getting secrets from GSM

--- integration-matrikkel/src/main/kotlin/no/kartverket/matrikkel/komreg/MatrikkelenhetEntitySource.kt
+++ integration-matrikkel/src/main/kotlin/no/kartverket/matrikkel/komreg/MatrikkelenhetEntitySource.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.flow
 import no.kartverket.komreg.core.KrAppBootContext
 import no.kartverket.komreg.core.data.RawData
 import no.kartverket.komreg.core.domain.Matrikkelnummer
+import no.kartverket.komreg.core.getSecretOrString
 import no.kartverket.komreg.integration.spi.SimpleEntitySource
 import no.kartverket.komreg.integration.spi.SimpleEntitySourceFactory
 import java.sql.DriverManager
@@ -54,9 +55,9 @@ class MatrikkelnrEntitySourceFactory : SimpleEntitySourceFactory<RawData<Matrikk
     override fun KrAppBootContext.create(): SimpleEntitySource<RawData<Matrikkelnummer>> {
         val matrikkelConfig = config.getConfig("integration.matrikkel")
         return MatrikkelenhetEntitySource(
-            matrikkelConfig.getString("jdbcUrl"),
-            matrikkelConfig.getString("user"),
-            matrikkelConfig.getString("password"),
+            matrikkelConfig.getSecretOrString("jdbcUrl"),
+            matrikkelConfig.getSecretOrString("user"),
+            matrikkelConfig.getSecretOrString("password"),
         )
     }
 }
