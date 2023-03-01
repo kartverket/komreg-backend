22e2f3379c6be55318516e216522305243930650
Christian <scheiehein@gmail.com>
2023-03-01 12:11:20 +0100

    Add logger

--- integration-matrikkel/build.gradle.kts
+++ integration-matrikkel/build.gradle.kts
@@ -3,7 +3,12 @@ plugins {
     `java-library`
 }
 
+val logbackVersion = "1.4.5"
+
+
 dependencies {
     api(project(":core-api"))
     runtimeOnly("com.oracle.database.jdbc:ojdbc11:21.7.0.0")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
 }
