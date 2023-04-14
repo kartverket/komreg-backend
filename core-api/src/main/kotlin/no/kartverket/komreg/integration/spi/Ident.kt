b81d9a2c79738ec079a48fe642885ad8348c3f57
Tor Egil Strand <strant@kartverket.no>
2023-04-14 09:50:26 +0200

    TKR-167: Endrer slik at man kan se tidligere ident

---
+++ core-api/src/main/kotlin/no/kartverket/komreg/integration/spi/Ident.kt
@@ -0,0 +1,47 @@
+package no.kartverket.komreg.integration.spi
+
+import java.util.stream.Collectors
+import kotlin.reflect.KClass
+import kotlin.streams.asStream
+
+class Ident private constructor(val map: Map<KClass<*>, *>) {
+    constructor(vararg values: Any) : this(
+        values.asSequence()
+            .asStream()
+            .collect(
+                Collectors.toMap(
+                    { it::class },
+                    { it }
+                )
+            )
+    )
+
+    inline fun <reified T> get(): T = map[T::class] as T
+
+    fun transform(transformation: Ident): Ident {
+        if (!map.keys.containsAll(transformation.map.keys)) {
+            val extra = transformation.map.keys - map.keys
+            throw IllegalArgumentException("Can not transform non-existing values: $extra")
+        }
+
+        return Ident(map.plus(transformation.map))
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Ident) return false
+
+        if (map != other.map) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return map.hashCode()
+    }
+
+    override fun toString(): String {
+        return "Ident(map=$map)"
+    }
+
+}
