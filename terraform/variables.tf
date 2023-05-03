da703d1be13c3ac118fefb9094af4e19bc054e0e
Christian H. Mosveen <christian.mosveen@kartverket.no>
2023-05-03 12:29:05 +0200

    Remove Kubernetes deploy from Terraform
    Initial step before move to ArgoCD

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,14 +6,6 @@ variable "project_id" {
   type = string
 }
 
-variable "image" {
-  type = string
-}
-
-variable "external_dns_hostname" {
-  type = string
-}
-
 variable "deploy_env" {
   type = string
 }
