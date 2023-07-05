08dfb7ee48c94840e064ffe566818c8e327b7465
dhlon <daniel.lonar@bekk.no>
2023-07-05 16:34:36 +0200

    Move creation of source list for local env to entitysourcemanager

--- transformation/src/main/kotlin/no/kartverket/komreg/transformation/EntitySourceManager.kt
+++ transformation/src/main/kotlin/no/kartverket/komreg/transformation/EntitySourceManager.kt
@@ -1,9 +1,6 @@
 package no.kartverket.komreg.transformation
 
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
 import no.kartverket.komreg.core.KrAppBootContext
-import no.kartverket.komreg.integration.spi.Entity
 import no.kartverket.komreg.integration.spi.EntitySource
 import no.kartverket.komreg.integration.spi.EntitySourceFactory
 import org.slf4j.Logger
@@ -14,7 +11,9 @@ class EntitySourceManager(bootContext: KrAppBootContext) {
 
     private val logger: Logger = LoggerFactory.getLogger(this::class.java)
 
-    val entitySources: List<EntitySource>
+    var entitySources: List<EntitySource>
+
+    private var isLocal = false
 
     init {
         val services = ServiceLoader.load(EntitySourceFactory::class.java)
@@ -26,9 +25,42 @@ class EntitySourceManager(bootContext: KrAppBootContext) {
         entitySources.forEach {
             logger.info("EntitySource: ${it.id} - $it")
         }
+
+        isLocal = System.getenv("environment") == "local" || System.getenv("environment") == null
+        logger.info("Current environment: ${System.getenv("environment")}")
+
+        // If running in local environment, flip the order of the sources, then flip the order of veg and vegadresse
+        if (isLocal) {
+            entitySources = createEntitySourcesForLocalEnv(bootContext)
+        }
     }
 
-    @OptIn(FlowPreview::class)
-    fun buildEntityFlow(): List<Pair<String, Flow<Entity>>> =
-        entitySources.map { Pair(it::class.toString(), it.entityFlow) }
+    private fun createEntitySourcesForLocalEnv(
+        bootContext: KrAppBootContext,
+    ): List<EntitySource> {
+        logger.info("Converting custom source order for local env")
+        logger.info("Flipping whole source list")
+        val sources = EntitySourceManager(bootContext).entitySources.reversed().toMutableList()
+        var vegIndex = -1
+        var vegadresseIndex = -1
+
+        for (i in sources.indices) {
+            val currentId = sources[i].id.lowercase()
+
+            if (currentId.contains("veg") && !currentId.contains("vegadresse") && vegIndex == -1) {
+                vegIndex = i
+            }
+            if (currentId.contains("vegadresse") && vegadresseIndex == -1) {
+                vegadresseIndex = i
+            }
+        }
+
+        if (vegIndex != -1 && vegadresseIndex != -1) {
+            logger.info("Swapping entities containing 'veg' and 'vegadresse' in source list")
+            val temp = sources[vegIndex]
+            sources[vegIndex] = sources[vegadresseIndex]
+            sources[vegadresseIndex] = temp
+        }
+        return sources
+    }
 }
