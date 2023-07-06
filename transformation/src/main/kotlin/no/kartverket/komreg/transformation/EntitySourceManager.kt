6a4c84e15b8005e0f7ad2328471129fc48876c65
dhlon <daniel.lonar@bekk.no>
2023-07-06 11:11:57 +0200

    Remove swapping of veg and vegadresse

--- transformation/src/main/kotlin/no/kartverket/komreg/transformation/EntitySourceManager.kt
+++ transformation/src/main/kotlin/no/kartverket/komreg/transformation/EntitySourceManager.kt
@@ -22,45 +22,16 @@ class EntitySourceManager(bootContext: KrAppBootContext) {
         entitySources = with(bootContext) {
             services.map { service -> with(service) { create() } }
         }
-        entitySources.forEach {
-            logger.info("EntitySource: ${it.id} - $it")
-        }
 
         isLocal = System.getenv("environment") == "local" || System.getenv("environment") == null
         logger.info("Current environment: ${System.getenv("environment")}")
 
-        // If running in local environment, flip the order of the sources, then flip the order of veg and vegadresse
         if (isLocal) {
-            entitySources = createEntitySourcesForLocalEnv(bootContext)
-        }
-    }
-
-    private fun createEntitySourcesForLocalEnv(
-        bootContext: KrAppBootContext,
-    ): List<EntitySource> {
-        logger.info("Converting custom source order for local env")
-        logger.info("Flipping whole source list")
-        val sources = EntitySourceManager(bootContext).entitySources.reversed().toMutableList()
-        var vegIndex = -1
-        var vegadresseIndex = -1
-
-        for (i in sources.indices) {
-            val currentId = sources[i].id.lowercase()
-
-            if (currentId.contains("veg") && !currentId.contains("vegadresse") && vegIndex == -1) {
-                vegIndex = i
-            }
-            if (currentId.contains("vegadresse") && vegadresseIndex == -1) {
-                vegadresseIndex = i
-            }
+            entitySources = entitySources.reversed()
         }
 
-        if (vegIndex != -1 && vegadresseIndex != -1) {
-            logger.info("Swapping entities containing 'veg' and 'vegadresse' in source list")
-            val temp = sources[vegIndex]
-            sources[vegIndex] = sources[vegadresseIndex]
-            sources[vegadresseIndex] = temp
+        entitySources.forEach {
+            logger.info("EntitySource: ${it.id} - $it")
         }
-        return sources
     }
 }
