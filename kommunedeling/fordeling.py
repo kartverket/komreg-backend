2785a9254a60fb75f6d246b05c2c96b5b931a93e
dhlon <daniel.lonar@bekk.no>
2023-10-20 14:58:22 +0200

    Fiks fordelingsscript: Endre default til-verdi til kommune 1

--- kommunedeling/fordeling.py
+++ kommunedeling/fordeling.py
@@ -1,10 +1,8 @@
 import json
 
-
 def save_json(data, filename):
     with open(filename, 'w', encoding='utf8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
 
 with open('fordelingsparametre.json', 'r', encoding='utf8') as f:
     fordelingsparametre = json.load(f)
@@ -15,11 +13,11 @@ with open('fordeling_input.json', 'r', encoding='utf8') as f:
 transformasjoner = []
 
 eksisterende_kommuneløpenummer = fordeling_input['eksisterende_kommuneløpenummer']
+kommune1 = fordeling_input['kommuneløpenummer1']
 fylkesnummer = fordeling_input['fylkesnummer']
 
 for gårdsnummer in fordelingsparametre['gårdsnumre']:
-    til_value = fordeling_input['matrikkelenhet'].get(
-        gårdsnummer, eksisterende_kommuneløpenummer)
+    til_value = fordeling_input['matrikkelenhet'].get(gårdsnummer, kommune1)
     transformasjoner.append({
         'type': 'matrikkelenhet',
         'fylkesnummer': {'fra': fylkesnummer, 'til': fylkesnummer},
@@ -28,8 +26,7 @@ for gårdsnummer in fordelingsparametre['gårdsnumre']:
     })
 
 for adresse in fordelingsparametre['adresseparseller']:
-    til_value = fordeling_input['veg'].get(
-        adresse['adressekode'], [eksisterende_kommuneløpenummer])
+    til_value = fordeling_input['veg'].get(adresse['adressekode'], [kommune1])
     transformasjoner.append({
         'type': 'veg',
         'fylkesnummer': {'fra': fylkesnummer, 'til': fylkesnummer},
@@ -38,8 +35,7 @@ for adresse in fordelingsparametre['adresseparseller']:
     })
 
 for krets in fordelingsparametre['kretser']:
-    til_value = fordeling_input['krets'].get(
-        krets['kretsnummer'], eksisterende_kommuneløpenummer)
+    til_value = fordeling_input['krets'].get(krets['kretsnummer'], kommune1)
     nytt_kretsnummer = krets.get('nytt_kretsnummer', krets['kretsnummer'])
     transformasjoner.append({
         'type': 'krets',
@@ -49,8 +45,7 @@ for krets in fordelingsparametre['kretser']:
     })
 
 for teig in fordelingsparametre['teiger']:
-    til_value = fordeling_input['teig'].get(
-        teig['teigId'], eksisterende_kommuneløpenummer)
+    til_value = fordeling_input['teig'].get(teig['teigId'], kommune1)
     transformasjoner.append({
         'type': 'teig',
         'fylkesnummer': {'fra': fylkesnummer, 'til': fylkesnummer},
