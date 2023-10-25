2e6f14001f7eb56cd64b896c152080cd687dee6c
Christian H. Mosveen <christian.mosveen@kartverket.no>
2023-10-25 08:30:50 +0200

    Forsøk på håndtering av sefrakminne

--- transformation/src/test/kotlin/no/kartverket/komreg/transformation/TransformerByggTest.kt
+++ transformation/src/test/kotlin/no/kartverket/komreg/transformation/TransformerByggTest.kt
@@ -4,6 +4,7 @@ import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
 import kotlinx.datetime.toKotlinLocalDate
+import kotlinx.serialization.builtins.serializer
 import no.kartverket.komreg.core.domain.*
 import no.kartverket.komreg.integration.spi.Entity
 import no.kartverket.komreg.integration.spi.IdGeneratorManager
@@ -70,7 +71,7 @@ class TransformerByggTest {
 
         val result = transformerEntity(reguleringsInput, entity, idGenerator)
 
-        val expectedIdent = Ident.Empty
+        val expectedIdent = identOfBygningUtenFylkeOgKommune(123456789)
         val expectedAssociatedIdents = setOf(identOfMatrikkelenhet(2, 6, 1))
         assertEquals(expectedIdent, result?.single()?.transformedIdent)
         assertEquals(expectedAssociatedIdents, result?.single()?.transformedAssociatedIdents)
@@ -85,7 +86,7 @@ class TransformerByggTest {
                 identOfMatrikkelenhet(3, 5, 1),
             ),
         )
-        val expectedIdent = Ident.Empty
+        val expectedIdent = identOfBygningUtenFylkeOgKommune(123456789)
         val result = transformerEntity(reguleringsInput, entity, idGenerator)
         val expectedAssociatedIdents = setOf(identOfMatrikkelenhet(4, 6, 1))
         assertEquals(expectedIdent, result?.single()?.transformedIdent)
@@ -104,7 +105,29 @@ class TransformerByggTest {
 
         val result = transformerEntity(reguleringsInput, entity, idGenerator)
 
-        val expectedAssociatedIdents = setOf(identOfMatrikkelenhet(4, 6, 1), Ident.Empty)
+        val expectedAssociatedIdents = setOf(
+            identOfMatrikkelenhet(4, 6, 1),
+            identOfBygningUtenFylkeOgKommune(123456789),
+        )
+        assertEquals(expectedAssociatedIdents, result?.single()?.transformedAssociatedIdents)
+    }
+
+    @Test
+    fun `A transformation of sefrakminne should set ident empty`() {
+        val entity = Entity(
+            id = sefrakId(123),
+            ident = identOfSefrakUnknown(3, 5, "X"),
+            associatedIdents = setOf(
+                identOfMatrikkelenhet(3, 5, 1),
+            ),
+        )
+
+        val result = transformerEntity(reguleringsInput, entity, idGenerator)
+
+        val expectedIdent = Ident.Empty
+        val expectedAssociatedIdents = setOf(identOfMatrikkelenhet(4, 6, 1))
+
+        assertEquals(expectedIdent, result?.single()?.transformedIdent)
         assertEquals(expectedAssociatedIdents, result?.single()?.transformedAssociatedIdents)
     }
 
@@ -125,4 +148,40 @@ class TransformerByggTest {
                 Bygningsnummer(bygningsnummer),
             )
         }
+
+    private fun identOfBygningUtenFylkeOgKommune(bygningsnummer: Long) =
+        runBlocking {
+            Ident(Bygningsnummer(bygningsnummer))
+        }
+
+    private fun identOfSefrakUnknown(fylkesnummer: Long, lopenummer: Int, x: String) =
+        runBlocking {
+            Ident(
+                Fylkesnummer(fylkesnummer),
+                Kommunenummer.Lopenummer(lopenummer.toByte()),
+                SefrakObjektnummerDummy(x),
+            )
+        }
+}
+
+data class SefrakObjektnummerDummy(
+    val value: String,
+) : Comparable<SefrakObjektnummerDummy> {
+    override fun compareTo(other: SefrakObjektnummerDummy) = value.compareTo(other.value)
+}
+
+fun sefrakId(value: Int): Id {
+    return Id(Sefrakminne, value)
+}
+
+object Sefrakminne : IdType<Int, Sefrakminne> {
+    override fun compareTo(other: Sefrakminne): Int {
+        return 0
+    }
+
+    override fun compare(o1: Int, o2: Int): Int {
+        return o1.compareTo(o2)
+    }
+
+    override val valueSerializer = Int.serializer()
 }
