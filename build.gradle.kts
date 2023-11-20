775bda0922d33d684bb8c078cd5fc41d518f176e
dhlon <daniel.lonar@bekk.no>
2023-11-20 08:56:42 +0100

    Fjern gammel reguleringsinputmapping

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,6 @@ plugins {
 allprojects {
     repositories {
         mavenCentral()
-        mavenLocal()
         maven {
             url = uri("https://maven.pkg.github.com/kartverket/matrikkel")
             credentials {
