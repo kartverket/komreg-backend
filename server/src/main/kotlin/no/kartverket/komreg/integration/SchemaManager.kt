d5f60075d80ff8bda77f4f0afe8ed5a953d5d685
dhlon <daniel.lonar@bekk.no>
2023-11-28 12:56:27 +0100

    New env variable setup

---
+++ server/src/main/kotlin/no/kartverket/komreg/integration/SchemaManager.kt
@@ -0,0 +1,32 @@
+package no.kartverket.komreg.integration
+
+class SchemaManager {
+
+    fun getMottakerUsername(): String {
+        val environment = System.getenv("environment")
+        return if (environment == "prod") {
+            selectProdMottakerUsername()
+        } else {
+            "DB_MATRIKKEL_MOTTAKER1_USERNAME"
+        }
+    }
+
+    fun getMottakerPassword(): String {
+        val environment = System.getenv("environment")
+        return if (environment == "prod") {
+            selectProdMottakerPassword()
+        } else {
+            "DB_MATRIKKEL_MOTTAKER1_PASSWORD"
+        }
+    }
+
+    fun selectProdMottakerUsername(): String {
+        // TODO: logikk for å velge mottakerskjema som er "READY"
+        return "DB_MATRIKKEL_MOTTAKER1_USERNAME"
+    }
+
+    fun selectProdMottakerPassword(): String {
+        // TODO: logikk for å velge mottakerskjema som er "READY"
+        return "DB_MATRIKKEL_MOTTAKER1_PASSWORD"
+    }
+}
