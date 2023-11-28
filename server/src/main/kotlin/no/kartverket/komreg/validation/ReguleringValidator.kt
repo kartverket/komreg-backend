598b26278df6fbf6e350d1a1c660d100dbc6ba6b
Christian Hein <31273371+chrhein@users.noreply.github.com>
2023-11-28 07:49:20 +0100

    Enkel validering for reguleringer (#182)

---
+++ server/src/main/kotlin/no/kartverket/komreg/validation/ReguleringValidator.kt
@@ -0,0 +1,109 @@
+package no.kartverket.komreg.validation
+
+import no.kartverket.komreg.routes.*
+
+enum class ErrorType {
+    FYLKESDELING_MANGLER_KOMMUNER,
+    KOMMUNEDELING_MANGLER_MATRIKKELENHETER,
+    KOMMUNEDELING_MANGLER_KRETSER,
+    KOMMUNEDELING_MANGLER_TEIGER,
+    KOMMUNEDELING_MANGLER_VEGER,
+    VEGDELING_MANGLER_VEGADRESSER,
+}
+
+class ReguleringValidator {
+
+    companion object {
+        fun validateFylkesdeling(regulering: Regulering): Map<String, List<ErrorType>> {
+            val errors = mutableMapOf<String, List<ErrorType>>()
+
+            regulering.endringer.forEach { endring ->
+                val isFylkesdeling = endring.transformasjoner.any {
+                    it is FylkeTransformasjonDTO && it.fylkesnummer.til.size > 1
+                }
+
+                if (isFylkesdeling && !hasKommunetransformasjon(endring)) {
+                    errors[endring.id] = listOf(ErrorType.FYLKESDELING_MANGLER_KOMMUNER)
+                }
+            }
+
+            return errors
+        }
+
+        fun validateKommunedeling(regulering: Regulering): Map<String, List<ErrorType>> {
+            val errors = mutableMapOf<String, List<ErrorType>>()
+
+            regulering.endringer.forEach { endring ->
+                val isKommunedeling = endring.transformasjoner.any {
+                    it is KommuneTransformasjonDTO && it.kommuneløpenummer.til.size > 1
+                }
+
+                if (isKommunedeling) {
+                    var existingErrors = errors.getOrDefault(endring.id, emptyList())
+
+                    if (!hasKretstrasformasjon(endring)) {
+                        existingErrors = existingErrors.plus(ErrorType.KOMMUNEDELING_MANGLER_KRETSER)
+                    }
+                    if (!hasMatrikkelenhettransformasjon(endring)) {
+                        existingErrors = existingErrors.plus(ErrorType.KOMMUNEDELING_MANGLER_MATRIKKELENHETER)
+                    }
+                    if (!hasTeigtransformasjon(endring)) {
+                        existingErrors = existingErrors.plus(ErrorType.KOMMUNEDELING_MANGLER_TEIGER)
+                    }
+                    if (!hasVegtransformasjon(endring)) {
+                        existingErrors = existingErrors.plus(ErrorType.KOMMUNEDELING_MANGLER_VEGER)
+                    }
+
+                    errors[endring.id] = existingErrors
+                }
+            }
+            return errors
+        }
+
+        fun validateVegdeling(regulering: Regulering): Map<String, List<ErrorType>> {
+            val errors = mutableMapOf<String, List<ErrorType>>()
+
+            regulering.endringer.forEach { endring ->
+                val isVegdeling = endring.transformasjoner.any {
+                    it is VegTransformasjonDTO && it.kommuneløpenummer.til.size > 1
+                }
+
+                if (isVegdeling && !hasVegadressetransformasjon(endring)) {
+                    errors[endring.id] = listOf(ErrorType.VEGDELING_MANGLER_VEGADRESSER)
+                }
+            }
+
+            return errors
+        }
+
+        fun validate(
+            regulering: Regulering
+        ): Map<String, List<ErrorType>> {
+            val errors = mutableMapOf<String, List<ErrorType>>()
+
+            errors.putAll(validateFylkesdeling(regulering))
+            errors.putAll(validateKommunedeling(regulering))
+            errors.putAll(validateVegdeling(regulering))
+
+            return errors
+        }
+
+        private fun hasKommunetransformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is KommuneTransformasjonDTO }
+
+        private fun hasKretstrasformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is KretsTransformasjonDTO }
+
+        private fun hasMatrikkelenhettransformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is MatrikkelenhetTransformasjonDTO }
+
+        private fun hasTeigtransformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is TeigTransformasjonDTO }
+
+        private fun hasVegtransformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is VegTransformasjonDTO }
+
+        private fun hasVegadressetransformasjon(endring: EndringDTO) =
+            endring.transformasjoner.any { it is VegadresseTransformasjonDTO }
+    }
+}
