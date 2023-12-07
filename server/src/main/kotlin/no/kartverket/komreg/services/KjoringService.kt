1fa83a7dcd6ea41299d7be19120ad9088d6efe2a
Mathias <mathias.fossum@kartverket.no>
2023-12-07 10:11:41 +0100

    TKR-429: Passer på at man ikke kan opprette ny kjøring på regulering før den første er ferdig

--- server/src/main/kotlin/no/kartverket/komreg/services/KjoringService.kt
+++ server/src/main/kotlin/no/kartverket/komreg/services/KjoringService.kt
@@ -3,6 +3,7 @@ package no.kartverket.komreg.services
 import no.kartverket.komreg.repositories.Kjoring
 import no.kartverket.komreg.repositories.KjoringRepo
 import no.kartverket.komreg.repositories.Kjoringstatus
+import java.lang.IllegalArgumentException
 
 class KjoringService(private val kjoringRepo: KjoringRepo) {
     fun handleShutdown() {
@@ -17,6 +18,13 @@ class KjoringService(private val kjoringRepo: KjoringRepo) {
     }
 
     fun opprettKjoring(reguleringId: String): Kjoring {
+        val harOpprettedeKjoringerPaaRegulering =
+            kjoringRepo.getStatusForKjoringMedReguleringsId(reguleringId).any { it.status == Kjoringstatus.OPPRETTET }
+
+        if (harOpprettedeKjoringerPaaRegulering) {
+            throw IllegalArgumentException("Det finnes allerede en kjøring på reguleringen som har status OPPRETTET")
+        }
+
         return kjoringRepo.opprettKjoring(reguleringId)
     }
 
