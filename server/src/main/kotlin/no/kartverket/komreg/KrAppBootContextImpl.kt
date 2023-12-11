ac9107f309e79a0817ad8d0cfaa077df91ad02f2
Christian H. Mosveen <christian.mosveen@kartverket.no>
2023-12-11 09:13:13 +0100

    Logger advarsel og setter default hvis miljøvariabel mangler

--- server/src/main/kotlin/no/kartverket/komreg/KrAppBootContextImpl.kt
+++ server/src/main/kotlin/no/kartverket/komreg/KrAppBootContextImpl.kt
@@ -7,21 +7,30 @@ import no.kartverket.komreg.integration.SchemaManager
 import java.util.Properties
 
 object KrAppBootContextImpl : KrAppBootContext {
-    val schemaManager = SchemaManager()
+    private val schemaManager = SchemaManager()
     override val config: Config =
         ConfigFactory
             .parseProperties(
                 Properties().apply {
-                    setProperty("integration.matrikkel.mottaker.jdbcUrl", env["DB_MATRIKKEL_JDBC_URL"])
-                    setProperty("integration.matrikkel.mottaker.user", env[schemaManager.getMottakerUsername()])
-                    setProperty("integration.matrikkel.mottaker.password", env[schemaManager.getMottakerPassword()])
-                    setProperty("integration.matrikkel.backing.jdbcUrl", env["DB_MATRIKKEL_JDBC_URL"])
-                    setProperty("integration.matrikkel.backing.user", env["DB_MATRIKKEL_BACKING_USERNAME"])
-                    setProperty("integration.matrikkel.backing.password", env["DB_MATRIKKEL_BACKING_PASSWORD"])
-                    setProperty("integration.matrikkel.system.jdbcUrl", env["DB_MATRIKKEL_JDBC_URL"])
-                    setProperty("integration.matrikkel.system.user", env["DB_MATRIKKEL_SYSTEM_USERNAME"])
-                    setProperty("integration.matrikkel.system.password", env["DB_MATRIKKEL_SYSTEM_PASSWORD"])
+                    setProperty("integration.matrikkel.mottaker.jdbcUrl", env("DB_MATRIKKEL_JDBC_URL"))
+                    setProperty("integration.matrikkel.mottaker.user", env(schemaManager.getMottakerUsername()))
+                    setProperty("integration.matrikkel.mottaker.password", env(schemaManager.getMottakerPassword()))
+                    setProperty("integration.matrikkel.backing.jdbcUrl", env("DB_MATRIKKEL_JDBC_URL"))
+                    setProperty("integration.matrikkel.backing.user", env("DB_MATRIKKEL_BACKING_USERNAME"))
+                    setProperty("integration.matrikkel.backing.password", env("DB_MATRIKKEL_BACKING_PASSWORD"))
+                    setProperty("integration.matrikkel.system.jdbcUrl", env("DB_MATRIKKEL_JDBC_URL"))
+                    setProperty("integration.matrikkel.system.user", env("DB_MATRIKKEL_SYSTEM_USERNAME"))
+                    setProperty("integration.matrikkel.system.password", env("DB_MATRIKKEL_SYSTEM_PASSWORD"))
                 },
             )
             .withFallback(ConfigFactory.defaultApplication())
+}
+
+private fun env(v: String): String {
+    val verdi = env[v]
+    if (verdi == null) {
+        logger.warn("Mangler miljøvariabel-verdi for $v")
+        return "ENV_MISSING"
+    }
+    return verdi
 }
