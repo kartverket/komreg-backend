bbd0907fbf7a2e64c5dbfa38c1429cb62ad21e82
Mathias <mathias.fossum@kartverket.no>
2023-12-11 12:24:02 +0100

    TKR-429: Endrer skjema til hva som er i mottaker

--- server/src/main/kotlin/no/kartverket/komreg/integration/SchemaManager.kt
+++ server/src/main/kotlin/no/kartverket/komreg/integration/SchemaManager.kt
@@ -5,32 +5,10 @@ import no.kartverket.komreg.repositories.KjoringRepo
 class SchemaManager(private val kjoringRepo: KjoringRepo) {
 
     fun getMottakerUsername(): String {
-        val environment = System.getenv("environment")
-
-        return if (environment === "prod") {
-            selectProdMottakerUsername()
-        } else {
-            "DB_MATRIKKEL_MOTTAKER2_USERNAME"
-            //kjoringRepo.hentMottakerSkjema().mottaker.toString() + "_USERNAME"
-        }
+        return kjoringRepo.hentMottakerSkjema().mottaker.toString() + "_USERNAME"
     }
 
     fun getMottakerPassword(): String {
-        val environment = System.getenv("environment")
-        return if (environment == "prod") {
-            selectProdMottakerPassword()
-        } else
-            "DB_MATRIKKEL_MOTTAKER2_PASSWORD"
-        //kjoringRepo.hentMottakerSkjema().mottaker.toString() + "_PASSWORD"
-    }
-
-    fun selectProdMottakerUsername(): String {
-        // TODO: logikk for å velge mottakerskjema som er "READY"
-        return "DB_MATRIKKEL_MOTTAKER1_USERNAME"
-    }
-
-    fun selectProdMottakerPassword(): String {
-        // TODO: logikk for å velge mottakerskjema som er "READY"
-        return "DB_MATRIKKEL_MOTTAKER1_PASSWORD"
+        return kjoringRepo.hentMottakerSkjema().mottaker.toString() + "_PASSWORD"
     }
 }
