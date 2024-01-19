2c251c531d867355f924d9eba4ca4f4c0ad3261c
Tor Egil Strand <strant@kartverket.no>
2024-01-19 14:35:27 +0100

    TKR-491: Fjerner valideringskonsept som aldri ble innført

--- transformation/src/test/kotlin/no/kartverket/komreg/transformation/MockSource.kt
+++ transformation/src/test/kotlin/no/kartverket/komreg/transformation/MockSource.kt
@@ -7,10 +7,6 @@ import no.kartverket.komreg.integration.spi.EntitySource
 
 class MockSource(override val entityFlow: Flow<Entity>) : EntitySource {
     override val id = "MockSource"
-    override val preValidation: Set<() -> Unit>
-        get() = TODO("Not yet implemented")
-    override val postValidation: Set<() -> Unit>
-        get() = TODO("Not yet implemented")
 }
 
 fun mockSource(vararg entities: Entity) = MockSource(entities.asFlow())
