4976800d90e3d81c76794e91e8d64cd852714646
Tor Egil Strand <strant@kartverket.no>
2024-02-08 09:10:59 +0100

    TKR-444: Ny versjon av matrikkel-komreg

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation(project(":transformation"))
 
     // Skulle egentlig vært runtimeOnly, men har ikke funnet på noe ordentlig grensesnitt for SSR-generering
-    implementation("no.statkart.matrikkel:matrikkel-komreg:4.15-SNAPSHOT") {
+    implementation("no.statkart.matrikkel:matrikkel-komreg:4.17-SNAPSHOT") {
         isChanging = true
         exclude(group = "com.oracle.database.jdbc")
     }
