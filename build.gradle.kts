b9acaa27dc61b1af9ee1f44b1a15f8e47e41082f
Alexander Solstad Larsen <489056+alexasl@users.noreply.github.com>
2024-09-05 19:58:14 +0200

    Fjernet unødvendig GH_USERNAME property/env-var/secret

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,17 @@ subprojects {
         mavenCentral()
         maven {
             url = uri("https://maven.pkg.github.com/kartverket/matrikkel")
-            credentials {
-                username = project.findProperty("GH_USERNAME") as String? ?: System.getenv("GH_USERNAME")
-                password = project.findProperty("GH_PACKAGES_PAT") as String? ?: System.getenv("GH_PACKAGES_PAT")
+            val matrikkelRepoToken = project.findProperty("GH_PACKAGES_PAT") as String? ?: System.getenv("GH_PACKAGES_PAT")
+            if (!matrikkelRepoToken.isNullOrBlank()) {
+                credentials(HttpHeaderCredentials::class) {
+                    name = "Authorization"
+                    value = "Bearer $matrikkelRepoToken"
+                }
+                authentication {
+                    create("header", HttpHeaderAuthentication::class)
+                }
+            } else {
+                logger.warn("GH_PACKAGES_PAT er ikke satt: Kan ikke hente avhengigheter fra GitHub Packages")
             }
         }
     }
