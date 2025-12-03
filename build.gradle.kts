b77199d1837a50e2a0eaffc22cae28a87c21bb88
Alexander Solstad Larsen <489056+alexasl@users.noreply.github.com>
2025-12-03 13:22:08 +0100

    TS-2496 Oppgrader avhengighet til Matrikkel - variant bygget i komreg-backend-repo
    Også oppgradert til Java 17, siden matrikkelen er bygget med det - og vil ikke kompilere uten

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,25 +11,10 @@ subprojects {
 
     repositories {
         mavenCentral()
-        maven {
-            url = uri("https://maven.pkg.github.com/kartverket/matrikkel")
-            val matrikkelRepoToken = project.findProperty("GH_PACKAGES_PAT") as String? ?: System.getenv("GH_PACKAGES_PAT")
-            if (!matrikkelRepoToken.isNullOrBlank()) {
-                credentials(HttpHeaderCredentials::class) {
-                    name = "Authorization"
-                    value = "Bearer $matrikkelRepoToken"
-                }
-                authentication {
-                    create("header", HttpHeaderAuthentication::class)
-                }
-            } else {
-                logger.warn("GH_PACKAGES_PAT er ikke satt: Kan ikke hente avhengigheter fra GitHub Packages")
-            }
-        }
     }
 
     plugins.withType<JavaBasePlugin> {
-        val javaVersion = JavaVersion.VERSION_11
+        val javaVersion = JavaVersion.VERSION_17
 
         extensions.configure<JavaPluginExtension> {
             sourceCompatibility = javaVersion
