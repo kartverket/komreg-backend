3e8252cfc15ea9d3ee9160f913fabbc35540e7a9
Aleksander Karlsen <aleksander.wk@proton.me>
2026-06-01 10:33:10 +0200

    Add use public dockerimage

--- lokal-dev/start-db.sh
+++ lokal-dev/start-db.sh
@@ -5,7 +5,12 @@ echo "Starting matrikkel-db (Oracle XE)..."
 if docker ps -a --format '{{.Names}}' | grep -q '^matrikkel-db$'; then
   docker start matrikkel-db
 else
-  docker run --name matrikkel-db -p 1521:1521 -p 5500:5500 -d nexus.statkart.no:8082/matrikkel-oracle-xe-onedb:latest
+  docker run --name matrikkel-db -p 1521:1521 -p 5500:5500 \
+    -e ORACLE_PASSWORD=Matrikkeladm4kv \
+    -e APP_USER=MATRIKKEL_DEV \
+    -e APP_USER_PASSWORD=MATRIKKEL_DEV \
+    -e ORACLE_DATABASE=devmatr1 \
+    -d gvenzl/oracle-xe:18.4.0
 fi
 
 echo "Starting komreg-db (PostgreSQL)..."
