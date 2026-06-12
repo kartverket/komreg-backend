14e69c235fdb983a08a45c6a18c7b32ad39e3800
Kristian <kristian.haugerud@kartverket.no>
2026-06-12 11:53:17 +0200

    chore: Fjern ubrukt spek avhengighet

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -127,8 +127,6 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.spek.dsl.jvm)
-    testRuntimeOnly(libs.spek.runner.junit5)
     testImplementation(libs.mockk)
     testImplementation(libs.hamcrest)
     testImplementation(libs.hamcrest.library)
