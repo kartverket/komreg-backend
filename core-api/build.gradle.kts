2f17be6cf7208ad18278df2ac0a04456b3b53290
Kristian <kristian.haugerud@kartverket.no>
2026-06-15 10:02:09 +0200

    Oppdater subproject med buildSrc

--- core-api/build.gradle.kts
+++ core-api/build.gradle.kts
@@ -1,32 +1,8 @@
-group = "no.kartverket.komreg"
 
 plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.plugin.serialization)
-    `maven-publish`
+    id("komreg.published-library")
 }
 
-java {
-    withSourcesJar()
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/kartverket/komreg-backend")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
-}
 
 dependencies {
     api(platform(libs.arrow.stack))
@@ -44,8 +20,4 @@ dependencies {
     testRuntimeOnly(libs.junit.jupiter.engine)
     testImplementation(libs.kotest.runner)
     testImplementation(libs.logback.classic)
-}
-
-tasks.named("test", Test::class).configure {
-    useJUnitPlatform()
 }
